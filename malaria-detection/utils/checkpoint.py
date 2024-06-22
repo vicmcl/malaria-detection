3ef48e0212d042d1785faaf33a2ab900f016b8a6
victorien <victorienmichel3@gmail.com>
2024-06-22 16:44:19 +0200

    FEAT: save best weights

---
+++ malaria-detection/utils/checkpoint.py
@@ -0,0 +1,15 @@
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from pathlib import Path
+
+def checkpoint(fname, patience = 5):
+    weights_path = Path('../weights')
+    callbacks = [
+        EarlyStopping(patiencec = patience),    # Stop the training after 20 iterations without improvement
+        ModelCheckpoint(                        # Save the weights giving the best results in a file
+            filepath = weights_path / fname,
+            monitor = "val_loss",
+            save_best_only = True,
+        )
+    ]
+
+    return callbacks
