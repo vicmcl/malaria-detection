3aeb25077dbbc57b2f39f07d389f3ab2fe10f96e
victorien <victorienmichel3@gmail.com>
2024-06-24 15:29:05 +0200

    FIX: set weight Path to str

--- malaria-detection/utils/checkpoint.py
+++ malaria-detection/utils/checkpoint.py
@@ -6,7 +6,7 @@ def checkpoint(fname, patience = 5):
     callbacks = [
         EarlyStopping(patience = patience),    # Stop the training after 20 iterations without improvement
         ModelCheckpoint(                        # Save the weights giving the best results in a file
-            filepath = weights_path / fname,
+            filepath = str(weights_path / fname),
             monitor = "val_loss",
             save_best_only = True,
         )
