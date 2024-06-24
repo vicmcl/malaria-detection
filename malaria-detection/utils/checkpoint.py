45ce817bf09f1baf3e899c9324a0eab8463e58ef
victorien <victorienmichel3@gmail.com>
2024-06-24 15:26:26 +0200

    FIX: set patience in EarlyStopping

--- malaria-detection/utils/checkpoint.py
+++ malaria-detection/utils/checkpoint.py
@@ -4,7 +4,7 @@ from pathlib import Path
 def checkpoint(fname, patience = 5):
     weights_path = Path('../weights')
     callbacks = [
-        EarlyStopping(patiencec = patience),    # Stop the training after 20 iterations without improvement
+        EarlyStopping(patience = patience),    # Stop the training after 20 iterations without improvement
         ModelCheckpoint(                        # Save the weights giving the best results in a file
             filepath = weights_path / fname,
             monitor = "val_loss",
