28413a3863b11743e9f2289ccf5c562cdcf0a1f9
victorien <victorienmichel3@gmail.com>
2024-06-26 17:45:57 +0200

    FEAT: custom color palette

--- malaria-detection/utils/plots.py
+++ malaria-detection/utils/plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import random
 import seaborn as sns
-
+from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 
 def plot_images(images, labels, n):
     # Set the figure size
@@ -56,6 +56,14 @@ def settings():
     plt.rcdefaults()
     sns.set_style('whitegrid')
 
+    colors = ['#FFFFFF', '#70f3a0', '#89939C', "#4C4C4C"]
+    
+    custom_palette = ListedColormap(colors)
+    plt.register_cmap(name='my_palette', cmap=custom_palette)
+
+    custom_cmap = LinearSegmentedColormap.from_list('custom', colors)
+    plt.register_cmap(name='my_cmap', cmap=custom_cmap)
+
     # Set custom figure parameters
     plt.rcParams.update({
         'axes.labelcolor':      '#4C4C4C',   # label parameters
@@ -80,4 +88,17 @@ def settings():
         'ytick.color':          "#89939C",
         'ytick.labelcolor':     "#89939C",
         'ytick.major.width':    1,
+
+        'legend.edgecolor':     '#4C4C4C',   # legend parameters
+        'legend.facecolor':     'white',
+        'legend.fontsize':      12,
+        'legend.title_fontsize': 14,
+        'legend.title_color':   '#4C4C4C',
+        'legend.labelcolor':    '#4C4C4C',
+
+        'axes.titlecolor':      '#4C4C4C',   # axis title color
+        'figure.titlesize':     22,          # figure title size
+        'figure.titleweight':   'bold',      # figure title weight
+        'figure.titlecolor':    '#4C4C4C',   # figure title color
+
     })
